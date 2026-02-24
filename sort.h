037066837b23ba3312241647925185c8904f9315
Rashad745 <rnuraliyev@gmail.com>
2026-02-24 05:53:30 +0000

    Fix listint_t definition in sort.h

--- sort.h
+++ sort.h
@@ -4,7 +4,23 @@
 #include <stddef.h>
 #include <stdio.h>
 
+/**
+ * struct listint_s - doubly linked list node
+ * @n: integer stored in the node
+ * @prev: pointer to the previous element
+ * @next: pointer to the next element
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif
